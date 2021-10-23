88f64312f2fd4b38f3d8fa21bbc31c1bb8b9b6cd
giovannicorte <giovanni.corte@icloud.com>
2021-10-23 17:13:40 +0200

    Initial Commit

---
+++ Nearme/NearmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearmeApp.swift
+//  Nearme
+//
+//  Created by Giovanni Corte on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NearmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
